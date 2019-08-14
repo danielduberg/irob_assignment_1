2b6de87c29c066c8753f5e2efd7306a4e8a67475
Daniel Duberg <danielduberg@gmail.com>
2019-08-14 16:54:32 +0200

    Added install script

---
+++ install.bash
@@ -0,0 +1,18 @@
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+
+sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+
+sudo apt update
+
+sudo apt install ros-melodic-desktop-full
+
+sudo rosdep init
+
+rosdep update
+
+sudo apt install python-rosinstall python-rosinstall-generator python-wstool build-essential python-pip python-catkin-tools
+
+sudo apt install ros-melodic-ros-tutorials ros-melodic-turtlebot3 ros-melodic-turtlebot3-simulations ros-melodic-navigation libspatialindex-dev libqt4-dev
+
+sudo apt install ros-melodic-rqt ros-melodic-rqt-common-plugins ros-melodic-turtlesim
+sudo apt install ros-melodic-turtle-tf2 ros-melodic-tf2-tools ros-melodic-tf
